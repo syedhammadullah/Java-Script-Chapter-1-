4488eba40fa2b165cfc879b9402226757b93404e
Syed Hammad Ullah <syedhammadullah660@gmail.com>
2024-03-05 00:48:12 +0500

    Exercise 1 Q(1-3)

---
+++ app.js
@@ -0,0 +1,17 @@
+// Chapter # 01:Alerts
+
+// Exercise 1:
+/* I'll give you one exercise at a time. After you give your answer for each exercise,
+           I'll give you another exercise. When you get one wrong, I'll let you know. Later in the session,
+            I'll circle back to that exercise to give you another try.
+
+            What is the keyword that displays a message box?*/
+// Answer = alert
+
+// Exercise 2:
+/* Type the character that ends every statement.*/
+//    Answer = ;
+
+// Exercise 3:
+/* Type the 2 characters that are missing in this statement.alert"You're learning JavaScript!";*/
+// Answer = ()
